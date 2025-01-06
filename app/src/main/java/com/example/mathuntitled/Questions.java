ad3f89d53e7939a9801fe963c9a02b77c735cd12
Franquelie Masanque <137916686+franquelie@users.noreply.github.com>
2025-01-06 21:01:34 +0800

    in progress

---
+++ app/src/main/java/com/example/mathuntitled/Questions.java
@@ -0,0 +1,23 @@
+package com.example.mathuntitled;
+
+public class Questions {
+
+    public static String question[] = {
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Where are you watching this video?"
+    };
+
+    public static String choices[][] = {
+            {"Google", "Apple", "Nokia", "Samsung"},
+            {"Java", "Kotlin", "Notepad", "Python"},
+            {"Facebook", "Whatsapp", "Instagram", "YouTube"}
+    };
+
+    public static String correctAnswer[]= {
+            "Google",
+            "Notepad",
+            "Youtube"
+    };
+
+}
